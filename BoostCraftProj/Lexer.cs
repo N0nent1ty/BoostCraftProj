2e21953dfafde529bdb368b8a7022f15dc0df403
cipher <shengchikuo@gmail.com>
2022-10-20 18:26:49 +0800

    Executable commit

---
+++ BoostCraftProj/Lexer.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+
+
+namespace Lexer_and_parser {
+
+    class TryParseFailException : Exception
+    {
+        public TryParseFailException() { }
+
+        public TryParseFailException(string strMessage)
+            : base(strMessage))
+        {
+
+        }
+    }
+
+
+    enum TOKEN_TYPES
+    {
+        OPEN,
+        CLOSE,
+        OPEN_WITH_SLASH,
+        IDENTIFIER
+    }
+
+
+
+    class cTOKEN_NODE{
+        private TOKEN_TYPES Token_type;
+        private string text;
+        public cTOKEN_NODE( TOKEN_TYPES n, string s) { 
+            this.Token_type = n;
+            this.text = s;
+        }
+        public TOKEN_TYPES get_type() { return this.Token_type; }
+        public string getText() { return this.text; }
+    }
+
+
+    //================================================================
+    //Class Lexer
+    //
+    //================================================================
+
+    class Lexer {
+        private string strInputString;
+        private int nCurrentCharPosition;
+        private int nInputLength;
+        private List<cTOKEN_NODE> tokens;
+
+        //Constructor of Lexer
+        public Lexer(string strInputString) { 
+            this.strInputString= strInputString;
+            //error handling with input length 0 problem.
+            if (string.IsNullOrEmpty(this.strInputString))
+                throw new ArgumentException("Lexer constructor error, invalid input, string can not be empty or null.", nameof(strInputString));
+
+            this.nCurrentCharPosition= 0;
+            this.nInputLength = strInputString.Length;
+            this.tokens = new List<cTOKEN_NODE>();
+        }
+
+
+        public List<cTOKEN_NODE> get_tokens() { return this.tokens; }
+
+        public char peek_One_Char() {
+            return this.strInputString[nCurrentCharPosition];
+        }
+
+
+
+        public void match_OPEN_TOKEN() {
+            ignore_space();
+            char ch = peek_One_Char();
+            char chOpen = '<';
+            //Console.WriteLine("ch is " + ch);
+            //Console.WriteLine(chOpen.Equals(ch));
+            if (chOpen.Equals(ch))
+            {
+                
+                nCurrentCharPosition += 1;
+                cTOKEN_NODE node = new cTOKEN_NODE(TOKEN_TYPES.OPEN, "<");
+                this.tokens.Add(node);
+                return;
+            }
+            else {
+                //handle not match error
+                string strErrorMessage = String.Format("Expect the  OPEN token but a {0}, not a valid XML node", ch);
+                throw new ApplicationException(strErrorMessage);
+            }
+        }
+
+        public void ignore_space() {
+            char ch;
+            // space or alphabet or digits
+            while(true)
+            {
+                ch = peek_One_Char();
+                if (Char.IsWhiteSpace(ch))
+                {
+                    nCurrentCharPosition++;
+                }
+                else {
+                    break;
+                }
+                if (this.nCurrentCharPosition == this.nInputLength)
+                {
+                    throw new ApplicationException("Expect a token but reach EOF, not a valid XML node");
+                }
+            };
+
+        }
+
+
+        public void match_IDENTIFY_TOKEN() {
+            ignore_space();
+            char ch ;
+            int nTokenBeginPosition = nCurrentCharPosition;
+            int nTokenLengthCount = 0;
+            //at least on identifier
+            bool bHasIdentifier = false;
+            while (true) {
+                ch = peek_One_Char();
+                if (Char.IsLetterOrDigit(ch))
+                {
+                    bHasIdentifier = true;
+                    nTokenLengthCount++;
+                    nCurrentCharPosition++;
+                }
+                else if (ch.Equals('>'))
+                {
+                    //Console.WriteLine(bHasIdentifier);
+                    if (bHasIdentifier == false) {
+                        string strErrorMessage = String.Format("Expect the  IDENTIFIER token but a CLOSE token \'>\' , not a valid XML node");
+                        throw new ApplicationException(strErrorMessage);
+                    }
+
+                    string s = strInputString.Substring(nTokenBeginPosition, nTokenLengthCount);
+                    cTOKEN_NODE Identifier_node = new cTOKEN_NODE(TOKEN_TYPES.IDENTIFIER, s);
+                    this.tokens.Add(Identifier_node);
+                    return;
+
+                }
+                else if (Char.IsWhiteSpace(ch)) {
+                    //try_parse_optional_attr();
+                    ignore_space();
+                    Console.WriteLine("Try parse attr");
+                
+                }
+                else
+                {
+                    //handle invalid symbol or alpahbet in Identifier.
+                    string strErrorMessage = String.Format("Expect the  IDENTIFIER token but a invalid character {0}, not a valid XML node", ch);
+                    throw new ApplicationException(strErrorMessage);
+                }
+            
+            }         
+        }
+
+        public void match_CLOSE_TOKEN() {
+            ignore_space();
+            char ch = peek_One_Char();
+            if (ch.Equals('>'))
+            {
+                cTOKEN_NODE close_node = new cTOKEN_NODE(TOKEN_TYPES.CLOSE, ">");
+                this.tokens.Add(close_node);
+            }
+            else {
+                string strErrorMessage = String.Format("Expect the  CLOSE token but a {0}, not a valid XML node", ch);
+                throw new ApplicationException(strErrorMessage);
+            }
+        }
+
+    }//end class lexer
+
+
+
+    //================================================================
+    //Class Parser
+    //
+    //================================================================
+    class Parser
+    {
+
+        private Lexer lexer;
+        public Parser(string strInput)
+        {
+            this.lexer = new Lexer(strInput);
+        }
+
+        public void Element()
+        {
+            try
+            {
+                this.lexer.match_OPEN_TOKEN();
+                this.lexer.match_IDENTIFY_TOKEN();
+                this.lexer.match_CLOSE_TOKEN();
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+        }
+        public List<cTOKEN_NODE> get_tokens() { return this.lexer.get_tokens(); }
+    }
+}
--- BoostCraftProj/Program.cs
+++ BoostCraftProj/Program.cs
@@ -1,55 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-using Antlr4.Runtime;
-using Antlr4.Runtime.Misc;
-using Antlr4.Runtime.Tree;
-using AntrlXMLParser;
-using AntlrCSharp;
-
+using Lexer_and_parser;
 
 
 class CheckFileFormat
 {
+
     static bool checkFileFormat(FileInfo file)
     {
         //If the input not even in XML node format, it simply raise a exeception 
-        try
-        {
 
-            //If the input format is a valid XML node, check it's Symmetry, ex: <person> </person>
-            System.Console.WriteLine("===================================================================");
-            System.Console.WriteLine("Execute the validator on " + file.Name);
-            System.Console.WriteLine("===================================================================");
-            string Input_string1 = System.IO.File.ReadAllText(file.FullName);
-            System.Console.WriteLine("Contents of text file = {0}", Input_string1);
+       //If the input format is a valid XML node, check it's Symmetry, ex: <person> </person>
+        System.Console.WriteLine("===================================================================");
+        System.Console.WriteLine("Execute the validator on " + file.Name);
+        System.Console.WriteLine("===================================================================");
+        string Input_string1 = System.IO.File.ReadAllText(file.FullName);
+        System.Console.WriteLine("Contents of text file = {0}", Input_string1);
+            
+        Lexer_and_parser.Parser parser = new Lexer_and_parser.Parser(Input_string1);
+        parser.Element();
 
-            AntlrInputStream inputStream = new AntlrInputStream(Input_string1);
-            XMLLexer inputLexer = new XMLLexer(inputStream);
-            CommonTokenStream commonTokenStream = new CommonTokenStream(inputLexer);
-            XMLParser inputParser = new XMLParser(commonTokenStream);
-
-            //Parse the file with XML node parser
-            XMLParser.ElementContext elementContext = inputParser.element();
-
-            //Create visitor to parse the node on the abstract syntax tree.
-            BasicXMLVisitor visitor = new BasicXMLVisitor();
-            visitor.Visit(elementContext);
-            if (visitor.bIsValidXMLNode)
-            {
-                Console.WriteLine(file.Name +" is a valid XML node\n");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine(file.Name + " is not a valid XML node\n");
-                return false;
-            }
-
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("This is not even a valid xml node, fail in parsing" + ex);
-        }
+        List<cTOKEN_NODE> tokens = parser.get_tokens();
+        foreach (cTOKEN_NODE token in tokens) {
+            Console.WriteLine(token.getText());
+        }        
         return false;
     }
 
